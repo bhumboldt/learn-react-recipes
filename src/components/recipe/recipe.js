1cf1483727b85b53a6dac040ce07d4ab97c298dc
Brandon Humboldt <brandon.humboldt@briebug.com>
2022-10-03 10:28:50 -0500

    Get deleting a recipe working

--- src/components/recipe/recipe.js
+++ src/components/recipe/recipe.js
@@ -1,13 +1,74 @@
-import { AccessTimeOutlined, Grade } from "@mui/icons-material";
-import { Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material"
+import { AccessTimeOutlined, Grade, MoreVert } from "@mui/icons-material";
+import { Card, CardContent, CardHeader, CardMedia, Typography, IconButton, Menu, MenuItem } from "@mui/material"
 import { Stack } from "@mui/system";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { removeRecipeThunk } from "../../state/recipes/recipe-thunks";
+import { DeleteConfirmationDialog } from "../delete-confirmation-dialog/delete-confirmation-dialog";
 import './recipe.css';
 
-export const Recipe = ({ name, description, cover_image, time }) => {
+export const Recipe = ({ name, description, cover_image, time, id }) => {
+  const dispatch = useDispatch();
+
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDeleteConfirmation = (shouldDelete) => {
+    if (shouldDelete) {
+      dispatch(removeRecipeThunk(id));
+    }
+
+    setDeleteDialogOpen(false);
+  }
+
+  const handleDeleteClicked = () => {
+    setAnchorEl(null);
+    setDeleteDialogOpen(true);
+  }
+
   return (
     <Card className="recipe">
       <CardHeader
         title={name}
+        action={
+          <>
+            <IconButton aria-label="settings" onClick={handleClick}>
+              <MoreVert />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+            >
+              <MenuItem>Edit</MenuItem>
+              <MenuItem color="error" onClick={handleDeleteClicked}>
+                <Typography variant="p" color="error">Delete</Typography>
+              </MenuItem>
+            </Menu>
+            <DeleteConfirmationDialog
+                  onClose={handleDeleteConfirmation}
+                  open={deleteDialogOpen}
+                  title="Delete Recipe?"
+                  description="Are you sure you want to delete this recipe?"
+                ></DeleteConfirmationDialog>
+          </>
+        }
       >
       </CardHeader>
       <CardMedia
